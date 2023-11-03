0bf47a20fbc4fb8f9badb5efe6e4b3d47a4166c0
ros2 <ros2@HN508001017636.cesar-ldap.intranet>
2023-11-03 17:43:04 -0400

    adicionando exercicio109

---
+++ atividade/src/atividade/exercici0110.java
@@ -0,0 +1,21 @@
+package atividade;
+
+public class exercici0110 {
+	
+	
+	public static void main(String[] args) {
+		
+		int numero = 0;
+		
+		do {
+			
+			System.out.println("numero " + numero);
+			numero++;
+			
+		} while(numero <= 110);
+		
+		
+		
+	}
+
+}
---
+++ atividade/src/atividade/exercicio109.java
@@ -0,0 +1,18 @@
+package atividade;
+
+public class exercicio109 {
+	
+	public static void main(String[] args) {
+		
+		int numero = 0;
+		
+		while (numero <= 10) {
+			
+			System.out.println("O número " + numero);
+			numero++;
+			
+		}
+		
+	}
+
+}
