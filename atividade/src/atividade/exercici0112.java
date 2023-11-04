8584f3a812a6fda050885ec1c8c196fedc7a6858
ros2 <ros2@HN508001017636.cesar-ldap.intranet>
2023-11-03 20:42:18 -0400

    adicionando exercici112.

---
+++ atividade/src/atividade/exercici0112.java
@@ -0,0 +1,19 @@
+package atividade;
+
+public class exercici0112 {
+	
+	public static void main(String[] args) {
+		
+		int num1 = 100;
+		int num2 = 100;
+		
+		if(num1 != num2) {
+			System.out.println("Verdade");
+		}
+		else {
+			System.out.println("Falso");
+		}
+		
+	}
+
+}
