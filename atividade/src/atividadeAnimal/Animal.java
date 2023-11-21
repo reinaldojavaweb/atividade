7f6413f88a6d21121e39f1a9008c5b2b08e190e0
ros2 <ros2@HN508001017636.cesar-ldap.intranet>
2023-11-21 09:52:05 -0400

    java web

---
+++ atividade/src/atividadeAnimal/Animal.java
@@ -0,0 +1,14 @@
+package atividadeAnimal;
+
+public class Animal {
+	
+	String name;
+	int idade;
+	String corPele;
+	String raca;
+	String nomeMae;
+	String nomePai;
+	String nomeIrmao;
+	String corPelo;
+
+}
